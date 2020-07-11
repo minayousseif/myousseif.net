4186334022e3b6bcfc7b59686d65681c4e75a2d1
Mina Yousseif <mina.yousseif@gmail.com>
2020-07-11 19:11:04 -0400

    add latest work experience and update packages

---
+++ src/data/experiences.js
@@ -0,0 +1,63 @@
+export const PROFESSIONAL_EXPERIENCE = [
+  {
+    id: 5,
+    fromYearLabel: "2020",
+    toYearLabel: "Current",
+    title: "Senior Software Developer",
+    company: "Deepwatch - Remote",
+    dateText: "Apr 2020-Current",
+    responsibilities: [
+      "Develop a cloud-based software application that compiles data and analytics on security programs and policies, including advance metrics and log source reporting, for use by existing and prospective customers.",
+    ]
+  },
+  {
+    id: 4,
+    fromYearLabel: "2016",
+    toYearLabel: "2020",
+    title: "Senior Software Engineer",
+    company: "Malwarebytes - Clearwater FL",
+    dateText: "Sept 2016- Apr 2020",
+    responsibilities: [
+      "Designing and building next-generation web applications and services supporting Malwarebytes products protecting customers against malware using Ruby, Ruby on Rails, Node.js, Python, Go, C++ and Amazon cloud computing services",
+      "Developed production-ready scalable microservice and serverless applications providing millions of clients with real-time information to help detect malware and prevent false positives."
+    ]
+  },
+  {
+    id: 3,
+    fromYearLabel: "2013",
+    toYearLabel: "2016",
+    title: "Web Developer",
+    company: "Daniels Corporation - Tampa FL",
+    dateText: "Nov 2013-Aug 2016",
+    responsibilities: [
+      "Developed and maintained the company's web applications utilizing C#, ASP.NET MVC pattern, Cold Fusion and Ruby on Rails.",
+      "Extended the usability and built a customized web client application and a REST Web API for SAP Business One using C#, ASP.NET MVC pattern and SAP DI Objects, which improved the SAP software out of box functionality, streamed line processes and resulting in a huge saving on licensing budget.",
+      "Created and integrated an EDI C# application with SAP Business One to add an automation layer and improved data transfer between all business units and the company's trading partners like Amazon, Wayfair, Ace Hardware, and Michaels."
+    ]
+  },
+  {
+    id: 2,
+    fromYearLabel: "2012",
+    toYearLabel: "2016",
+    title: "Freelance Web Developer",
+    company: "Minabyte LLC - Tampa Bay Area FL",
+    dateText: "Sept 2012 - Feb 2016",
+    responsibilities: [
+      "Developed and designed web applications including mobile web applications, customized shopping carts with eBay integration using C# ASP.NET, PHP, HTML, CSS, Bootstrap, jQuery, jQuery mobile, and the open-source shopping cart OpenCart.",
+      "Provided a complete IT service for contracted small businesses.",
+    ]
+  },
+  {
+    id: 1,
+    fromYearLabel: "2010",
+    toYearLabel: "2012",
+    title: "IT Support Specialist",
+    company: "Mensch Supply - NY",
+    dateText: "Nov 2010 - Aug 2012",
+    responsibilities: [
+      "Provided software and hardware troubleshot support, server maintenance, and developed C# server applications and scripts to automate reports and tasks. Troubleshooting technical issues and assisted employees with network problems, installed encryption solutions, PKI ECA digital certificates, medium USB token assurance, and PGP encryption applications, maintained LAN/WAN environment and configured VPN networks between company's locations.",
+      "Expedited the shipping process and saved hours of manual label creation by creating a Military MSL 129 marking and shipping label application to meet the DOD standards and requirements which eliminate the confusion and frustration of getting the right MIL-STD-129 compliant shipping labels.",
+      "Helped improving delivery tracking after developing a delivery logging and tracking application that was a middleware between the GPS fleet tracking system and the backend system."
+    ]
+  },
+];
